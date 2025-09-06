3699f049f2d76daeb42219bc9594b128e006e6db
pullinux <rockytriton@yahoo.com>
2025-09-05 22:50:13 -0500

    updates

--- build/push.sh
+++ build/push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 f=$1
-curl -X POST -H "Authorization: token $PLX_TOKEN" -H "Content-Type: application/x-tar"         --data-binary @$f "https://uploads.github.com/repos/Pullinux/pullinux/releases/243626789/assets?name=$f"
+curl -X POST -H "Authorization: token $PLX_TOKEN" -H "Content-Type: application/x-tar"         --data-binary @$f "https://uploads.github.com/repos/Pullinux/pullinux/releases/245204894/assets?name=$f"
