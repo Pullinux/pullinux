19f53251d2c0bcab25bacd2bc936ff8d62e9d368
pullinux <rockytriton@yahoo.com>
2025-09-05 23:02:42 -0500

    update to use older kernel

--- build/packages/l/linux/build.sh
+++ build/packages/l/linux/build.sh
@@ -9,12 +9,12 @@ make INSTALL_MOD_PATH=$PCKDIR modules_install
 
 mkdir -p $PCKDIR/boot
 
-cp -iv arch/x86/boot/bzImage $PCKDIR/boot/vmlinuz-6.16.1-plx-1.1-systemd
-cp -iv .config $PCKDIR/boot/config-6.16.1
+cp -iv arch/x86/boot/bzImage $PCKDIR/boot/vmlinuz-6.13.4-plx-1.1-systemd
+cp -iv .config $PCKDIR/boot/config-6.13.4
 
 make clean
 
 cd ..
 mkdir -p $PCKDIR/usr/src
-cp -r linux-6.16.1 $PCKDIR/usr/src/
+cp -r linux-6.13.4 $PCKDIR/usr/src/
 
