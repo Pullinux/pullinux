fa78b8abb969ca66f8e07b19c18916744ef2cbba
Rocky Pulley <rockytriton@yahoo.com>
2026-03-04 02:36:13 -0600

    Cleanup and fix some installer issues

--- src/utils/prep.sh
+++ src/utils/prep.sh
@@ -1,6 +1,2 @@
-export LFS=/mnt/lfs2
-export PLX=/mnt/lfs2
 export PLX_PCK_PATH=/home/rpulley/git/pullinux/packages
 export EDITOR=code
-sudo -E bash ../../mount-virt.sh 
-
--- umount-virt.sh
+++
@@ -1,6 +0,0 @@
-echo "${LFS:?}"
-
-mountpoint -q $LFS/dev/shm && umount $LFS/dev/shm
-umount $LFS/dev/pts
-umount $LFS/{sys,proc,run,dev}
-
